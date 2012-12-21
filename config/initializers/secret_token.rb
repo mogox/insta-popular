fca457b8801a9397dfbbad86861b0d4284243a4e
Enrique Carlos Mogollan <emogollan@gmail.com>
2012-12-21 12:07:04 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+InstaPopular::Application.config.secret_token = '8874d0b2ba980c4e994e4a345cc5a4707dc7e75c61d70da59af9d38fcac01420192d8359377cb84b53f9d58cedcbd91fca8cf92994de1e26c233a5dcfb5bb8f8'
